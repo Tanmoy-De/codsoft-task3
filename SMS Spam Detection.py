6fb9c1dd1ca13b9022435d4d74cb930437ddb7e8
Tanmoy-De <142774474+Tanmoy-De@users.noreply.github.com>
2023-08-25 10:58:33 +0530

    Add files via upload

---
+++ SMS Spam Detection.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+# In[4]:
+
+
+# Reading the data
+df = pd.read_csv("C:\\Users\\TANMOY DE\\Downloads\\spam.csv",encoding='latin-1')
+df.head()
+
+
+# In[5]:
+
+
+df = df.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
+df = df.rename(columns={'v1':'label','v2':'Text'})
+df['label_enc'] = df['label'].map({'ham':0,'spam':1})
+df.head()
+
+
+# In[6]:
+
+
+sns.countplot(x=df['label'])
+plt.show()
+
+
+# In[7]:
+
+
+# Find average number of tokens in all sentences
+avg_words_len=round(sum([len(i.split()) for i in df['Text']])/len(df['Text']))
+print(avg_words_len)
+
+
+# In[9]:
+
+
+# Finding Total no of unique words in corpus
+s = set()
+for sent in df['Text']:
+    for word in sent.split():
+        s.add(word)
+    total_words_length=len(s)
+print(total_words_length)
+
+
+# In[10]:
+
+
+# Splitting data for Training and testing
+from sklearn.model_selection import train_test_split
+
+X, y = np.asanyarray(df['Text']), np.asanyarray(df['label_enc'])
+new_df = pd.DataFrame({'Text': X, 'label': y})
+X_train, X_test, y_train, y_test = train_test_split(
+	new_df['Text'], new_df['label'], test_size=0.2, random_state=42)
+X_train.shape, y_train.shape, X_test.shape, y_test.shape
+
+
+# In[11]:
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report,accuracy_score
+
+tfidf_vec = TfidfVectorizer().fit(X_train)
+X_train_vec,X_test_vec = tfidf_vec.transform(X_train),tfidf_vec.transform(X_test)
+
+baseline_model = MultinomialNB()
+baseline_model.fit(X_train_vec,y_train)
+
+
+# In[13]:
+
+
+nb_accuracy = accuracy_score(y_test, baseline_model.predict(X_test_vec))
+print(nb_accuracy)
+print(classification_report(y_test, baseline_model.predict(X_test_vec)))
+
+
+# In[ ]:
+
+
+
+
